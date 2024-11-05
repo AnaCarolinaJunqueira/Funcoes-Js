2975ada4a67930a48c933c9afe713d230b63afb7
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-11-05 19:07:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Boa noite")
+
+function batata (){
+    console.log("Arroz")
+}
+
+batata()
+
+let frutas = "Morango"
+
+function mostrarFruta(frut){
+    console.log(frut)
+}
+
+mostrarFruta(frutas)
+
+function info(nome, idade, cidade){
+    console.log(nome)
+    console.log(idade)
+    console.log(cidade)
+}
+
+info("Vini", 24, "Salvador")
+
+function soma(n1,n2){
+    return n1 + n2
+}
+
+console.log(soma(2,6))
+
+const boasVindas = (nome) =>{
+    console.log(nome)
+}
+
+boasVindas("Vinicius")
+
+const soma = (n1, n2) => n1 + n2
+
+console.log(soma(2,5))
